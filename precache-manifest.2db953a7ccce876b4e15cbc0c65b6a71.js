e34ebe74b4242c44a14104dd3cca2c90fbcea43c
vilyamskaya <vilyamskaya.anna@gmail.com>
2021-05-10 21:05:12 +0300

    deploy

---
+++ precache-manifest.2db953a7ccce876b4e15cbc0c65b6a71.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98598409b4d05e9216f3",
+    "url": "/todoListApp/css/app.38f5bafd.css"
+  },
+  {
+    "revision": "8ba765ce9580a3bdae76b59130d0ef8a",
+    "url": "/todoListApp/fonts/AmaticSC-Bold.8ba765ce.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/todoListApp/fonts/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "ae8ca10018e728c4a5d62d2678426b47",
+    "url": "/todoListApp/img/check-dark.ae8ca100.svg"
+  },
+  {
+    "revision": "c92d190480348cdc60fe8fa79f28cf99",
+    "url": "/todoListApp/img/check-white.c92d1904.svg"
+  },
+  {
+    "revision": "4879ee9171f3d618598e9e8dcc4733c6",
+    "url": "/todoListApp/img/garbage.4879ee91.svg"
+  },
+  {
+    "revision": "64cfa0d48769f377c881060c15b4510e",
+    "url": "/todoListApp/img/pencil.64cfa0d4.svg"
+  },
+  {
+    "revision": "8adc5cd57e986c06bc51dc7fd0c49f04",
+    "url": "/todoListApp/img/remove.8adc5cd5.svg"
+  },
+  {
+    "revision": "430e596d335400f353963fbe09779349",
+    "url": "/todoListApp/index.html"
+  },
+  {
+    "revision": "98598409b4d05e9216f3",
+    "url": "/todoListApp/js/app.837716a5.js"
+  },
+  {
+    "revision": "8714e1b79608efed02ac",
+    "url": "/todoListApp/js/chunk-vendors.fedd26a5.js"
+  },
+  {
+    "revision": "15f0cd5bd7dcc5985f2d88ccc70a7c9b",
+    "url": "/todoListApp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/todoListApp/robots.txt"
+  }
+]);
